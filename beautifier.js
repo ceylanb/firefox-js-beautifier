970ed4f9c0ad9f6718ae5ebcdadae2a8b434084e
ceylanb <b0dy@b00m.localdomain>
2020-05-01 06:46:56 +0300

    first version

---
+++ beautifier.js
@@ -0,0 +1,11 @@
+var pre = document.getElementsByTagName('pre')[0];
+js_code = pre.innerText;
+pre.innerText = "";
+var code = document.createElement("code");
+code.classList.add("javascript");
+pre.appendChild(code)
+var source_code = js_beautify(js_code);
+code.innerText = source_code;
+hljs.highlightBlock(code);
+document.body.style.backgroundColor = window.getComputedStyle(code).backgroundColor
+
