41dd9b5212cad60da9d0c237dc5299c0991ff984
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-05-28 23:58:01 +0200

    Add condition to beautify htmls with only one < pre > node
    The algorithmus was applied to any url with .js at the ending.
    The led to an inteference at pages that aren't (at least not only)
    js codes, like github pages from files that end with .js.
    This condition filters the urls, applying the beautification
    only to htmls that have one 'pre' tag and nothing else.
    
    Resolves: #3

--- beautifier.js
+++ beautifier.js
@@ -1,10 +1,15 @@
-var pre = document.getElementsByTagName('pre')[0];
-js_code = pre.innerText;
-pre.innerText = "";
-var code = document.createElement("code");
-code.classList.add("javascript");
-pre.appendChild(code)
-var source_code = js_beautify(js_code);
-code.innerHTML = source_code;
-hljs.highlightBlock(code);
-document.body.style.backgroundColor = window.getComputedStyle(code).backgroundColor
+if (document.body.childNodes.length === 1 &&
+	document.body.children[0].nodeName === 'PRE') {
+
+  var pre = document.getElementsByTagName('pre')[0];
+  js_code = pre.innerText;
+  pre.innerText = "";
+  var code = document.createElement("code");
+  code.classList.add("javascript");
+  pre.appendChild(code)
+  var source_code = js_beautify(js_code);
+  code.innerHTML = source_code;
+  hljs.highlightBlock(code);
+  document.body.style.backgroundColor = window.getComputedStyle(code).backgroundColor
+
+}
