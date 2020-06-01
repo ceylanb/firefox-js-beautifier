d099620b22547adb17546526b91f7ac7203136c9
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-01 21:51:55 +0200

    Fix typo

--- beautifier.js
+++ beautifier.js
@@ -2,7 +2,7 @@ if (document.body.childNodes.length === 1 &&
   document.body.children[0].nodeName === 'PRE' &&
   document.body.children[0].children.length === 0 &&
   document.head.children.length === 1 &&
-  ddocument.head.children[0].rel === "stylesheet") {
+  document.head.children[0].rel === "stylesheet") {
 
   var pre = document.getElementsByTagName('pre')[0];
   js_code = pre.innerText;
